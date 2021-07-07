ccd73d1eebdd1ae6d07e2f1e7c9209602a2cd803
Syed Nazar Kazmi <syed.nazar9966@gmail.com>
2021-07-07 02:47:18 -0400

    Update setup.py
    update again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='metagenomics_deep_project',
+    name='DeepArg',
     version='1.0.2',
     packages=find_packages(
         exclude=(".git", "data")
