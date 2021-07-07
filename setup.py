945df9d05371c5a869f24c021f50582a18bd8df7
Syed Nazar Kazmi <syed.nazar9966@gmail.com>
2021-07-07 03:11:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'tqdm',
         'nolearn==0.6',
         'lasagne==0.1',
-        'scikit-learn==0.19.1',
+        'scikit-learn==0.17',
         'theano==0.8.2',
         'requests==2.18.4',
         'wget'
